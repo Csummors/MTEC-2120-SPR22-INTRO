f5d862906188b397451cad74607472fbfbb975cc
Csummors <60412568+Csummors@users.noreply.github.com>
2022-03-04 10:52:59 -0500

    fix enemy AI

---
+++ Assets/EnemeySpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemeySpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemyPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-5, 5), 1.0f, Random.Range(-5, 5));
+            Instantiate(enemyPrefab, position, Quaternion.identity);
+        }
+    }
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+   private void Start()
     {
-        
+        Enemy enemy = new Enemy();
+        GameObject temp = enemy.bulletPrefab;
     }
 
     // Update is called once per frame
